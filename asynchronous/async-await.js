a83b03522e30692c91fb60c6bd5c1d2c75d8449e
Suraj <suraj.gehlot92@gmail.com>
2020-09-21 13:40:53 +0530

    update

--- asynchronous/async-await.js
+++ asynchronous/async-await.js
@@ -1,28 +1,27 @@
 const getDogPic = async () => {
-    try {
-      const data = await readFilePro(`${__dirname}/dog.txt`);
-      console.log(`Breed: ${data}`);
-  
-      const res1Pro = superagent.get(
-        `https://dog.ceo/api/breed/${data}/images/random`
-      );
-      const res2Pro = superagent.get(
-        `https://dog.ceo/api/breed/${data}/images/random`
-      );
-      const res3Pro = superagent.get(
-        `https://dog.ceo/api/breed/${data}/images/random`
-      );
-      const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
-      const imgs = all.map(el => el.body.message);
-      console.log(imgs);
-  
-      await writeFilePro('dog-img.txt', imgs.join('\n'));
-      console.log('Random dog image saved to file!');
-    } catch (err) {
-      console.log(err);
-  
-      throw err;
-    }
-  };
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
 
-  getDogPic();
+    const res1Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro("dog-img.txt", imgs.join("\n"));
+    console.log("Random dog image file!");
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+getDogPicture();
